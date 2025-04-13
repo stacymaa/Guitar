45a630e1ede73be708acc13e2659147fb4f99fd2
«stacymaa» <stacyma.amk@gmail.com>
2025-04-14 02:19:25 +0300

    add steps

--- src/index.js
+++ src/index.js
@@ -15,3 +15,19 @@ const swiper = new Swiper('.slider-teacher', {
   },
   modules: [Navigation, Pagination],
 });
+
+// для работы accordion__btn
+var acc = document.getElementsByClassName("accordion__btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+ acc[i].addEventListener("click", function() {
+   this.classList.toggle("active");
+   var panel = this.nextElementSibling;
+   if (panel.style.maxHeight) {
+     panel.style.maxHeight = null;
+   } else {
+     panel.style.maxHeight = panel.scrollHeight + "px";
+   }
+ });
+}
